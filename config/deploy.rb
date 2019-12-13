05aa0011d2251468bb7e628829d6a4de3da1f0cc
yuya <yuevo16@gmail.com>
2019-12-13 15:12:41 +0900

    update3

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat_space''
+set :application, 'chat_space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yuevo16/chat_space.git'
